f7542d72d46f97779acefd001fe74671f768d820
Scott Hammer <shammer64@gmail.com>
2022-09-29 19:10:40 -0400

    SH Implemented turn 1

--- src/main/java/GoWinGame.java
+++ src/main/java/GoWinGame.java
@@ -1,5 +1,21 @@
 public class GoWinGame {
+
     public String evaluate(int turnNumber) {
-        return "1";
+        String response = "";
+        if (isDivisibleBy3(turnNumber)) {
+            response += "Go";
+        }
+        if (isDivisibleBy5(turnNumber)) {
+            response += "Win";
+        }
+        return response.isEmpty() ? String.valueOf(turnNumber) : response;
+    }
+
+    private boolean isDivisibleBy5(int turnNumber) {
+        return turnNumber % 5 == 0;
+    }
+
+    private boolean isDivisibleBy3(int turnNumber) {
+        return turnNumber % 3 == 0;
     }
 }
--- src/test/java/GoWinTest.java
+++ src/test/java/GoWinTest.java
@@ -1,15 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoWinTest {
 
+    private GoWinGame game;
+
+    @BeforeEach
+    public void setup() {
+        game = new GoWinGame();
+    }
+
     @Test
     public void turnOneShouldSayOne() {
-        GoWinGame game = new GoWinGame();
-
-        String response = game.evaluate(1);
-
-        assertEquals("1", response);
+        assertEquals("1", game.evaluate(1));
     }
+
+    @Test
+    public void turnTwoShouldSayTwo() {
+        assertEquals("2", game.evaluate(2));
+    }
+
+    @Test
+    public void turnThreeShouldSayGo() {
+        assertEquals("Go", game.evaluate(3));
+    }
+
+    @Test
+    public void turnFourShouldSayFour() {
+        assertEquals("4", game.evaluate(4));
+    }
+
+    @Test
+    public void turnFiveShouldSayWin() {
+        assertEquals("Win", game.evaluate(5));
+    }
+
+    @Test
+    public void turnSixShouldSayGo() {
+        assertEquals("Go", game.evaluate(6));
+    }
+
+    @Test
+    public void turnTenShouldSayWin() {
+        assertEquals("Win", game.evaluate(10));
+    }
+
+    @Test
+    public void turnFifteenShouldSayGoWin() {
+        assertEquals("GoWin", game.evaluate(15));
+    }
+
+    @Test
+    public void turnThirtyShouldSayGoWin() {
+        assertEquals("GoWin", game.evaluate(30));
+    }
+
 }
