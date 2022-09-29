a02702c192fa36342ba1b1ee999991d9dfe30950
Scott Hammer <shammer64@gmail.com>
2022-09-29 14:20:08 -0400

    Initial commit

---
+++ src/test/java/GoWinTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GoWinTest {
+
+    @Test
+    public void shouldBeBar() {
+        String response = "foo";
+        assertEquals("bar", response);
+    }
+}
